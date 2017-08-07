e473257cf249c20c3d314d6f03eee3ae5b071b79
reza <reza.cse08@gmail.com>
2017-08-08 02:41:04 +0600

    - create web api and other project - add user models

---
+++ ThreadingDemo/ThreadingDemo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing;
+
+namespace ThreadingDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartThreadButton = new System.Windows.Forms.Button();
+            this.ThreadResultLabel = new System.Windows.Forms.Label();
+            this.TaskStartButton = new System.Windows.Forms.Button();
+            this.TaskResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartThreadButton
+            // 
+            this.StartThreadButton.Location = new System.Drawing.Point(119, 122);
+            this.StartThreadButton.Margin = new System.Windows.Forms.Padding(4);
+            this.StartThreadButton.Name = "StartThreadButton";
+            this.StartThreadButton.Size = new System.Drawing.Size(100, 28);
+            this.StartThreadButton.TabIndex = 0;
+            this.StartThreadButton.Text = "Start Thread";
+            this.StartThreadButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.StartThreadButton.UseVisualStyleBackColor = true;
+            this.StartThreadButton.Click += new System.EventHandler(this.Starthread_OnClick);
+            // 
+            // ThreadResultLabel
+            // 
+            this.ThreadResultLabel.AutoSize = true;
+            this.ThreadResultLabel.Location = new System.Drawing.Point(145, 163);
+            this.ThreadResultLabel.Name = "ThreadResultLabel";
+            this.ThreadResultLabel.Size = new System.Drawing.Size(0, 17);
+            this.ThreadResultLabel.TabIndex = 1;
+            this.ThreadResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TaskStartButton
+            // 
+            this.TaskStartButton.Location = new System.Drawing.Point(119, 5);
+            this.TaskStartButton.Name = "TaskStartButton";
+            this.TaskStartButton.Size = new System.Drawing.Size(100, 28);
+            this.TaskStartButton.TabIndex = 2;
+            this.TaskStartButton.Text = "Start Task";
+            this.TaskStartButton.UseVisualStyleBackColor = true;
+            this.TaskStartButton.Click += new System.EventHandler(this.StartTask_OnClick);
+            // 
+            // TaskResultLabel
+            // 
+            this.TaskResultLabel.AutoSize = true;
+            this.TaskResultLabel.Location = new System.Drawing.Point(145, 48);
+            this.TaskResultLabel.Name = "TaskResultLabel";
+            this.TaskResultLabel.Size = new System.Drawing.Size(31, 17);
+            this.TaskResultLabel.TabIndex = 3;
+            this.TaskResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.TaskResultLabel);
+            this.Controls.Add(this.TaskStartButton);
+            this.Controls.Add(this.ThreadResultLabel);
+            this.Controls.Add(this.StartThreadButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Threading Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartThreadButton;
+        private System.Windows.Forms.Label ThreadResultLabel;
+        private System.Windows.Forms.Button TaskStartButton;
+        private System.Windows.Forms.Label TaskResultLabel;
+    }
+}
+
---
+++ ThreadingDemo/ThreadingDemo/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadingDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private int ProcessingSomething()
+        {
+            Thread.Sleep(5000);
+            return 1;
+        }
+        
+        private void Starthread_OnClick(object sender, EventArgs e)
+        {
+            StartThreadButton.Enabled = false;
+            var thread = new Thread(() =>
+            {
+                var count = ProcessingSomething();
+                var action = new Action<int>(PrintResult);
+                this.BeginInvoke(action, count);
+            });
+
+            thread.Start();
+            ThreadResultLabel.Text = "Processing thread...";
+        }
+
+        private void PrintResult(int result)
+        {
+            ThreadResultLabel.Text = "Thread Result : " + result;
+            StartThreadButton.Enabled = true;
+        }
+        
+        private async void StartTask_OnClick(object sender, EventArgs e)
+        {
+            TaskStartButton.Enabled = false;
+            var task  = new Task<int>(ProcessingSomething);
+            task.Start();
+
+            TaskResultLabel.Text = "Processing anync task...";
+
+            var count = await task;
+
+            TaskResultLabel.Text = "Task Result: " + count;
+            TaskStartButton.Enabled = true;
+        }
+        
+    }
+}
